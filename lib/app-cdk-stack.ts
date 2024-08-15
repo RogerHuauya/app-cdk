ec093c813ebf3c992a39e3b6251f263169158ce9
Roger <rhuauyam@uni.pe>
2024-08-14 23:05:27 -0500

    first commit

--- lib/app-cdk-stack.ts
+++ lib/app-cdk-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Role } from 'aws-cdk-lib/aws-iam';
+
+
 
 export class AppCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const account = cdk.Stack.of(this).account;
 
-    // The code that defines your stack goes here
+    const roleArn = cdk.Stack.of(this).formatArn({
+      service: 'iam',
+      account: account,
+      resource: 'role/LabRole',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myFunction = new lambda.Function(this, "HelloWorldFunction", {
+      runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+      `),
+      role: Role.fromRoleArn(this, "LambdaExecutionRole", roleArn),
+    });
+
+    // Define the Lambda function URL resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    // Define a CloudFormation output for your URL
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    })
   }
 }
